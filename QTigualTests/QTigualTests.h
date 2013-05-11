2601e8d390db9c3277a540ff35d70ae1a4d2f0b0
Roberto <roberto.martinezmn@gmail.com>
2013-05-11 16:29:09 -0500

    Initial Commit

---
+++ QTigualTests/QTigualTests.h
@@ -0,0 +1,13 @@
+//
+//  QTigualTests.h
+//  QTigualTests
+//
+//  Created by Roberto  on 5/11/13.
+//  Copyright (c) 2013 Roberto . All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QTigualTests : SenTestCase
+
+@end
---
+++ QTigualTests/QTigualTests.m
@@ -0,0 +1,32 @@
+//
+//  QTigualTests.m
+//  QTigualTests
+//
+//  Created by Roberto  on 5/11/13.
+//  Copyright (c) 2013 Roberto . All rights reserved.
+//
+
+#import "QTigualTests.h"
+
+@implementation QTigualTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in QTigualTests");
+}
+
+@end
